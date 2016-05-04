5f90f331cb124599bd0a57534f913298a7b71592
Necasova Klara <xnecas24@merlin.fit.vutbr.cz>
2016-05-04 15:04:20 +0200

    ICP: pridani strategii hry

--- ICP/new/Strategy.cpp
+++ ICP/new/Strategy.cpp
@@ -2,6 +2,7 @@
 #include "Strategy.h"
 #include <memory>
 #include "GameBoard.h"
+#include <time>
 
 using namespace std;
 
@@ -14,7 +15,20 @@ Alg1::Alg1(shared_ptr<GameBoard> board)
 	this->board = board;
 }
 
-int Alg1::executeMove(TPoint* point) {
+int Alg1::executeMove(TPoint* point)
+{
+	int max = 0;
+	int deskSize = board->getDeskSize();
+	vector <TPoint> points;
+	for(int i = 0; i < deskSize; i++) {
+		for(int j = 0; j < deskSize; j++) {
+			points = findingMoves({i, j}, BLACK);
+			if(points.size() > max) {
+				point.x = i;
+				point.y = j;
+			}
+		}
+	}
 	return 10;
 }
 
@@ -25,10 +39,25 @@ Alg2::Alg2(shared_ptr<GameBoard> board)
 }
 
 int Alg2::executeMove(TPoint* point) {
-	int x = 5, y = 4;
+	/*int x = 5, y = 4;
 	cout << "SURADNICE PC:::";
 	cin >>x; cin >> y;
 	point->x = x; point->y = y;
 	cout << board->getDeskSize();
+	return 20;*/
+
+	int max = 0;
+	int deskSize = board->getDeskSize();
+	vector <TPoint> points;
+	vector <TPoint> pointsPossib;
+	for(int i = 0; i < deskSize; i++) {
+		for(int j = 0; j < deskSize; j++) {
+			points = findingMoves({i, j}, BLACK);
+			if(points.size() > 0) {
+				pointsPossib.push_back({i, j});
+			}
+		}
+	}
+	point = pointsPossib[rand() % deskSize];
 	return 20;
 }
