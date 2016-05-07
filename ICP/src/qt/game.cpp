7f83527bfcd5f070690c51554765e881763b4cf6
Peter Tisovcik <xtisov00@stud.fit.vut.cz>
2016-05-07 20:37:45 +0200

    ICP: pridane komentare square.*

--- ICP/src/qt/game.cpp
+++ ICP/src/qt/game.cpp
@@ -1,112 +1,109 @@
-#include "game.h"
+#include <QGraphicsDropShadowEffect>
 #include <QMessageBox>
 #include <QObject>
-#include <QGraphicsDropShadowEffect>
-
+#include "game.h"
 
 Game::Game(int windowSize)
 {
-    scene = new QGraphicsScene();
-    this->windowSize = windowSize;
+	scene = new QGraphicsScene();
+	this->windowSize = windowSize;
 }
 
 void Game::getPosition(int x, int y) {
-    emit moveToPosition(x, y);
+	emit moveToPosition(x, y);
 }
 
 void Game::placeSquare(int deskSize)
 {
-    //vypocet velkosti stvorceka
-    float size = windowSize/deskSize;
+	//vypocet velkosti stvorceka
+	float size = windowSize/deskSize;
 
-    //vytvorenie hraciu dosku zo stvorcov
-    for (int j = 0; j < deskSize; j++)
-    {
-        for (int i = 0; i < deskSize; i++) {
-            Square* square = new Square(__null, size);
-            square->setPos(size*j,size*i);
-            squares.append(square);
-            scene->addItem(square);
-            connect(square, &Square::getClickPosition, this, &Game::getPosition);
-        }
-    }
+	//vytvorenie hraciu dosku zo stvorcov
+	for (int j = 0; j < deskSize; j++) {
+		for (int i = 0; i < deskSize; i++) {
+			Square* square = new Square(__null, size);
+			square->setPos(size*j,size*i);
+			squares.append(square);
+			scene->addItem(square);
+			connect(square, &Square::getClickPosition, this, &Game::getPosition);
+		}
+	}
 }
 
 void Game::createStone(int xPos, int yPos, Qt::GlobalColor  color, int deskSize)
 {
-    //vypocet velkosti
-    float size = this->windowSize/deskSize;
+	//vypocet velkosti
+	float size = this->windowSize/deskSize;
 
-    QGraphicsDropShadowEffect * effect = new QGraphicsDropShadowEffect();
-    effect->setBlurRadius(size/2);
+	QGraphicsDropShadowEffect * effect = new QGraphicsDropShadowEffect();
+	effect->setBlurRadius(size/2);
 
-    //pozicia kamena
-    float x = 3 + size * xPos;
-    float y = 3 + size * yPos;
-    QGraphicsEllipseItem *ell = new QGraphicsEllipseItem(x, y, size-6,size-6);
+	//pozicia kamena
+	float x = 3 + size * xPos;
+	float y = 3 + size * yPos;
+	QGraphicsEllipseItem *ell = new QGraphicsEllipseItem(x, y, size-6,size-6);
 
-    //ulozenie kamena, pre neskorsie odstranenie
-    if (color == Qt::darkCyan) {
-        hints.append(ell);
-    } else
-        stones.append(ell);
+	//ulozenie kamena, pre neskorsie odstranenie
+	if (color == Qt::darkCyan)
+		hints.append(ell);
+	else
+		stones.append(ell);
 
-    QBrush br(color);
-    ell->setGraphicsEffect(effect);
-    ell->setBrush(br);
+	QBrush brush(color);
+	ell->setGraphicsEffect(effect);
+	ell->setBrush(brush);
 
-    scene->addItem(ell);
+	scene->addItem(ell);
 }
 
 void Game::createStone(int x, int y, Qt::GlobalColor  color)
 {
-    QGraphicsDropShadowEffect * effect = new QGraphicsDropShadowEffect();
-    effect->setBlurRadius(45/2);
+	QGraphicsDropShadowEffect * effect = new QGraphicsDropShadowEffect();
+	effect->setBlurRadius(45/2);
 
-    QGraphicsEllipseItem *ell = new QGraphicsEllipseItem(x, y, 45,45);
+	QGraphicsEllipseItem *ell = new QGraphicsEllipseItem(x, y, 45,45);
 
-    if (color == Qt::darkCyan) {
-        hints.append(ell);
-    }
+	if (color == Qt::darkCyan)
+		hints.append(ell);
 
-    QBrush br(color);
-    ell->setBrush(br);
-    ell->setGraphicsEffect(effect);
+	QBrush brush(color);
+	ell->setBrush(brush);
+	ell->setGraphicsEffect(effect);
 
-    scene->addItem(ell);
+	scene->addItem(ell);
 
-    lastElipseItem = ell;
+	lastElipseItem = ell;
 }
 
 void Game::drawStone(std::shared_ptr <GameManager> board)
 {
-    int deskSize = board->getDeskSize();
+	int deskSize = board->getDeskSize();
 
-    //odstranenie starych kamenov
-    for (QGraphicsEllipseItem *item : hints)
-           scene->removeItem(item);
+	//odstranenie starych kamenov
+	for (QGraphicsEllipseItem *item : hints)
+		scene->removeItem(item);
 
-    for (QGraphicsEllipseItem *item : stones)
-           scene->removeItem(item);
+	for (QGraphicsEllipseItem *item : stones)
+		scene->removeItem(item);
 
-    hints.clear();
-    stones.clear();
+	hints.clear();
+	stones.clear();
 
-    //vykreslenie novych kamenov
-    for (int i = 0; i < deskSize; i++) {
-        for (int j = 0; j < deskSize; j++) {
-            if (board->getStone({i, j}) == WHITE)
-                createStone(i,j, Qt::white, deskSize);
-            else if(board->getStone({i, j}) == BLACK)
-                createStone(i,j, Qt::black, deskSize);
-            else if (board->getStone({i, j}) == 8)
-                createStone(i,j, Qt::darkCyan, deskSize);
-        }
-    }
+	//vykreslenie novych kamenov
+	for (int i = 0; i < deskSize; i++) {
+		for (int j = 0; j < deskSize; j++) {
+			if (board->getStone({i, j}) == WHITE)
+				createStone(i,j, Qt::white, deskSize);
+			else if(board->getStone({i, j}) == BLACK)
+				createStone(i,j, Qt::black, deskSize);
+			else if (board->getStone({i, j}) == 8)
+				createStone(i,j, Qt::darkCyan, deskSize);
+		}
+	}
 }
 
 void Game::setActivePlayer(int x, int y, Qt::GlobalColor color)
 {
-    delete lastElipseItem;
-    createStone(x, y, color);
+	delete lastElipseItem;
+	createStone(x, y, color);
 }
--- ICP/src/qt/game.h
+++ ICP/src/qt/game.h
@@ -2,39 +2,83 @@
 #define GAME
 
 #include <QList>
-#include "square.h"
 #include <QGraphicsScene>
 #include <QGraphicsSceneMouseEvent>
+#include "square.h"
 #include "GameManager.h"
 
 class Game : public QObject {
-    Q_OBJECT
+	Q_OBJECT
+
+	QList <Square*> squares;
+	QList <QGraphicsEllipseItem *> stones;
+	QList <QGraphicsEllipseItem *> hints;
+	QGraphicsEllipseItem *lastElipseItem;
+	int windowSize;
+
 public:
-    Game(int windowsSize);
+	QGraphicsScene* scene;
 
-    void placeSquare(int deskSize);
+	/**
+	 * Constructor hry, ktory vytvori hraciu scenu
+	 * @param	windowSize	Velkos hracieho okna
+	 */
+	Game(int windowSize);
 
-    QGraphicsScene* scene;
-    void createStone(int xPos, int yPos, Qt::GlobalColor  color , int deskSize);
-    void createStone(int xPos, int yPos, Qt::GlobalColor  color);
-    void drawStone(std::shared_ptr<GameManager> board);
-    void setActivePlayer(int x, int y, Qt::GlobalColor color);
-    QList <QGraphicsEllipseItem *> hints;
+	/**
+	 * Vytvorenie hracich policok na zaklade hracej dosky
+	 * @param	deskSize	Velkost hracej dosky
+	 */
+	void placeSquare(int deskSize);
+
+	/**
+	 * Vytvorenie kamena a umiestnenie na zadane index hracej dosky na
+	 * zaklade velkosti hracej dosky, na zaklade, ktorej sa vypocita
+	 * velkost kamena
+	 * @param	xPos		X index hracej dosky, kde sa ma umiestnit kamen
+	 * @param	yPos		Y index hracej dosky, kde sa ma umiestnit kamen
+	 * @param	color		Farba kamena
+	 * @param	deskSize	Velkost hracej dosky
+	 */
+	void createStone(int xPos, int yPos, Qt::GlobalColor  color , int deskSize);
+
+	/**
+	 * Vytvorenie kamena a umiestnenie na zadane x, y suradni hracej plochy
+	 * @param	x			X suradnica hracej plochy, kde sa ma umiestnit kamen
+	 * @param	y			Y suradnica hracej plochy, kde sa ma umiestnit kamen
+	 * @param	color		Farba kamena
+	 */
+	void createStone(int x, int y, Qt::GlobalColor  color);
+
+	/**
+	 * Vykreslenie kamenov na hraciu dosku
+	 * @param	board		Hracia doska
+	 */
+	void drawStone(std::shared_ptr<GameManager> board);
+
+	/**
+	 * Vykreslenie kamena, ktory znaci farbu aktualneho hraca
+	 * @param	x			X suradnica hracej plochy, kde sa ma umiestnit kamen
+	 * @param	y			Y suradnica hracej plochy, kde sa ma umiestnit kamen
+	 * @param	color		Farba kamena
+	 */
+	void setActivePlayer(int x, int y, Qt::GlobalColor color);
 
 public slots:
-    void getPosition(int x, int y);
+	/**
+	 * Slot na, ktory sa napoji signal po kliknuti na dany stvorec
+	 * @param	x			X index hracej dosky
+	 * @oaram	Y			Y index hracej dosky
+	 */
+	void getPosition(int x, int y);
 
 signals:
-    void moveToPosition(int x, int y);
-
-private:
-    void createHexColumn(int x, int y, int numHexes);
-    QList <Square*> squares;
-
-    QList <QGraphicsEllipseItem *> stones;
-    QGraphicsEllipseItem * lastElipseItem;
-    int windowSize;
+	/**
+	 * Signal pre stlacene suradnice s triedy Square
+	 * @param	x			X index hracej dosky
+	 * @oaram	Y			Y index hracej dosky
+	 */
+	void moveToPosition(int x, int y);
 };
 
 #endif // GAME
-
