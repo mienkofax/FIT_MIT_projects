c830b0a3e4c93619a029dd31ca8f8aee9441b530
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2016-10-06 23:42:47 +0200

    ISA: inicializacny commit z mainom a skriptami
    Pridane subory pre kompilaciu programu pomocou CMake.
    Pridany jendodychy main.
    
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

---
+++ ISA2/main.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file main.cpp
+ * @Author Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
+ * @date October, 2016
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char * argv[])
+{
+	return 0;
+}
