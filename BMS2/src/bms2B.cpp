40ea6fb631ae209774e821360dd1514f9d401cad
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2017-12-02 13:45:20 +0100

    BMS2: init project
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

--- BMS2/src/bms2B.cpp
+++ BMS2/src/bms2B.cpp
@@ -1,6 +1,34 @@
-#include <stdlib.h>
+/* 
+ * File:   bms1B.cpp
+ */
 
-int main()
-{
-	return EXIT_SUCCESS;
+#include <cstdlib>
+#include <math.h>
+
+#include "sndfile.cpp"
+
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    SndfileHandle inputFile;
+    int sampleRate;
+    int *buffer;
+    
+    inputFile = SndfileHandle("sine.waw");
+    
+    sampleRate = inputFile.samplerate();
+    
+    buffer = new int[sampleRate];
+
+    inputFile.read(buffer, sampleRate);
+    
+    
+    delete [] buffer;
+    return EXIT_SUCCESS;
 }
+
